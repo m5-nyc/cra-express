5734b62e37ae7344065cd99358b436317bc3f3fa
Christopher Maverick <cmaverickce@gmail.com>
2017-04-28 10:28:30 -0400

    set up basic cra with express

---
+++ server/routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.json([{
+        id: 1,
+        username: "NewYorkMets"
+    }, {
+        id: 2,
+        username: "NewYorkYankees"
+    }]);
+});
+
+
+
+module.exports = router;
